dea68499909acab09604f4a91af1853141fcb37d
Jorge Morana <gmorana@gmail.com>
2020-06-01 16:02:01 -0500

    Options on versioning

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -20,6 +20,10 @@ export interface TicketDoc extends mongoose.Document {
 // function for Ticket Model for Mongoose
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
+  findbyEvent(event: {
+    id: string;
+    version: number;
+  }): Promise<TicketDoc | null>;
 }
 
 const ticketSchema = new mongoose.Schema(
@@ -45,7 +49,19 @@ const ticketSchema = new mongoose.Schema(
 );
 ticketSchema.set("versionKey", "version");
 ticketSchema.plugin(updateIfCurrentPlugin);
-
+// ticketSchema.pre("save", function (done) {
+//   // @ts-ignore
+//   this.$where = {
+//     version: this.get("version") - 1,
+//   };
+//   done();
+// });
+ticketSchema.statics.findbyEvent = (event: { id: string; version: number }) => {
+  return Ticket.findOne({
+    _id: event.id,
+    version: event.version - 1,
+  });
+};
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket({
     _id: attrs.id,
