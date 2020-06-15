44f30f702547d68ad5d93efc146f32ed8cb4aed8
Jorge Morana <gmorana@gmail.com>
2020-06-15 16:11:54 -0500

    update two on the baseURL

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,20 +1,20 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default ({ req }) => {
-  if (typeof window === "undefined") {
+  if (typeof window === 'undefined') {
     // Server side
     // namespace: ingress-nginx
     // servicename: ingress-nginx-controller
     // http://servicename.namespace.svc.cluster.local
 
     return axios.create({
-      baseURL: "http://www.ticketing-dev.xyz",
+      baseURL: 'http://www.ticketing-dev.xyz/',
       headers: req.headers,
     });
   } else {
     // Browser side
     return axios.create({
-      baseURL: "/",
+      baseURL: '/',
     });
   }
 };
